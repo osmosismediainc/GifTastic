2b3d382855b424f74f6fd598218e155ef6df5464
osmosismediainc <osmosismediainc@gmail.com>
2019-08-24 20:32:44 -0400

    first functioning prototype

--- assets/js/giftastic.js
+++ assets/js/giftastic.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
 
     //var subjects = ['cat', 'crash', 'new york']
 
-    var topics = ['cat', 'crash', 'new york']
+    var topics = ['Cat', 'Crash', 'New York']
     // Function for rendering buttons
     function renderButtons() {
         $('#topics').empty();
         for (var i = 0; i < topics.length; i++) {
-            var buttons = $('<button type="button" id="gifButton" class="gifButton btn btn-primary">' + topics[i] + '</button>').attr("data-id",topics[i]);
+            var buttons = $('<button type="button" id="gifButton" class="gifButton btn btn-primary">' + topics[i] + '</button>').attr("data-id", topics[i]);
             buttons.appendTo('#topics');
         }
     }
@@ -21,33 +21,69 @@ $(document).ready(function () {
         renderButtons();
         console.log(topics);
     }
-    // Function for generation of gifs from button click
+    // Function for generation of gifs from button clickMaking new buttons 
     function makeGifs() {
     }
-    $( "#submit" ).click(function() {
+    $("#submit").click(function () {
         getNewQuery();
-      });
+    });
+    // Function for getting the query from the buttons
+    function getQuery() {
 
+    }
 
     // Render GIFS from button click
-     // Event listener for all button elements
-     //$('.buttons').on("click", function() {
-        
-        $(document).on('click', '.gifButton', function(){
-        
-        // In this case, the "this" keyword refers to the button that was clicked
-        // Assign to window (document) and class not ID
-
-        
-     var query = $(this).data("id");
-    console.log("Button Query: " + query);
-
-    // const a = document.querySelectorAll.this('.gifButton').innerHtml;
-    // console.log("Button Query: " + a);
+    // Event listener for query buttons
+    $(document).on('click', '.gifButton', function () {
+        var query = $(this).data("id"); // Grab the data-id from this button
+        console.log("Button Query: " + query); // Confirm the setting of the variable
+        // Constructing a URL to search Giphy for the name of the person who said the quote
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            query + "&api_key=sW3YM86CXwhhQAr9Gboj3SjindqphpzB&limit=10";
+        // Performing our AJAX GET request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After the data comes back from the API
+        .then(function(response) {
+            // Storing an array of results in the results variable
+            var results = response.data;
+  
+            // Looping over every result item
+            for (var i = 0; i < results.length; i++) {
+  
+              // Only taking action if the photo has an appropriate rating
+              if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                // Creating a div for the gif
+                var gifDiv = $("<div>");
+  
+                // Storing the result item's rating
+                var rating = results[i].rating;
+  
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + rating);
+  
+                // Creating an image tag
+                var giphyGif = $("<img>");
+  
+                // Giving the image tag an src attribute of a proprty pulled off the
+                // result item
+                giphyGif.attr("src", results[i].images.fixed_height.url);
+  
+                // Appending the paragraph and giphyGif we created to the "gifDiv" div we created
+                gifDiv.append(p);
+                gifDiv.append(giphyGif);
+  
+                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                $("#gifsAppearHere").prepend(gifDiv);
+              }
+            }
+          });
     });
 
 
-// First run of button rendering
+    // First run of button rendering
     renderButtons();
 
 
@@ -56,5 +92,5 @@ $(document).ready(function () {
 
 
 
-    
+
 });
